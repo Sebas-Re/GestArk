b588db4cbe84be1e0d6c02be4a413f51db7b2b6c
Squoshy <32151264+Squoshy-Boop@users.noreply.github.com>
2021-06-29 16:36:56 -0300

    Update MenuProveedores.h

--- MenuProveedores.h
+++ MenuProveedores.h
@@ -297,7 +297,7 @@ void seccionProveedores(){
                 int agregados=0;
                 system("cls");
                 agregados = agregarProveedor();
-                cout << agregados <<"APROVEEDORES/s CARGADO/s CON EXITO!"<<endl;
+                cout << agregados <<"PROVEEDORES/s CARGADO/s CON EXITO!"<<endl;
                 system("pause");
                 break;
             }
